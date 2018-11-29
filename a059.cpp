63599b88f4a4095fe3c66786414390be49794c64
simba-fs <simba.fs@gamil.com>
2018-11-29 17:05:31 +0800

    a059

--- a059.cpp
+++ a059.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 int main(){
   int n=0;
   int ab[100][2]={{}};
   cin  >> n;
-  for (int i=0;i<n;i++) cin >> ab[i][0] >> ab[i][1];
-  
+  for (int i=0;i<n;i++){
+    float a,b;
+    cin  >>a>>b;
+    a=sqrt(a);
+    b=sqrt(b);
+    cout <<a<<' '<<b<<endl;
+  }
 }
