979ec403bec8d6a5ce3df0b82134d267d4aba6a7
simba-fs <simba.fs@gamil.com>
2018-11-30 15:24:45 +0800

    finish a059

--- a059.cpp
+++ a059.cpp
@@ -5,12 +5,19 @@ int main(){
   int n=0;
   int ab[100][2]={{}};
   cin  >> n;
-  for (int i=0;i<n;i++){
+ 	for (int i=0;i<n;i++) cin>>ab[i][0]>>ab[i][1];
+	for (int i=0;i<n;i++){
     float a,b;
-    cin  >>a>>b;
+    int re=0;
+		a=ab[i][0];
+		b=ab[i][1];
     a=sqrt(a);
     b=sqrt(b);
-    cout <<a<<' '<<b<<endl;
+		for(int i=((int)a==a?a:a+1);i<=b;i++){
+			re+=i*i;
+		}
+		cout <<"Case " << i+1 << ": " <<  re << endl;
   }
+	return 0;
 }
 
