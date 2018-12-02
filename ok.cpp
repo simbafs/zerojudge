db357444705a05816b816251512d16274b9853d2
simba-fs <simba.fs@gamil.com>
2018-12-02 22:51:40 +0800

    add ok

--- ok.cpp
+++ ok.cpp
@@ -8,7 +8,7 @@ int main(int argc,char* argv[]){
 	char c[999999]={};
 	char ans;
 	if(argc>1){
-		for(int i=0;i<argc;i++) sprintf(m,"%s",argv[i]);
+		for(int i=1;i<argc;i++) sprintf(m,"%s %s",m,argv[i]);
 	}else{
 		sprintf(m,"change some files");
 	}
@@ -16,8 +16,8 @@ int main(int argc,char* argv[]){
 	cout << ">git add *" << endl;
 	cout << ">" << c << endl;
 	cout <<">y/n? ";
-	cin >> ans;
-	if(ans=='y' || 'Y'){
+	cin  >> ans;
+	if(ans=='y' || ans=='Y'){
 		system("git add *");
 		system(c);
 	}
