8ca446fb027ab70732aceeeeb16b7fd03f960c2e
simba-fs <simba.fs@gamil.com>
2018-12-02 22:41:05 +0800

    test

--- ok.cpp
+++ ok.cpp
@@ -3,13 +3,23 @@
 
 using namespace std;
 
-int main(){
-	char m[19132]={};
-	char c[20000]={};
-	cout << "commit massage? ";
-	cin  >>m;
-	system("git add *");
+int main(int argc,char* argv[]){
+	char m[999999]={};
+	char c[999999]={};
+	char ans;
+	if(argc>1){
+		for(int i=0;i<argc;i++) sprintf(m,"%s",argv[i]);
+	}else{
+		sprintf(m,"change some files");
+	}
 	sprintf(c,"git commit -m \"%s\"",m);
-	system(c);
+	cout << ">git add *" << endl;
+	cout << ">" << c << endl;
+	cout <<">y/n? ";
+	cin >> ans;
+	if(ans=='y' || 'Y'){
+		system("git add *");
+		system(c);
+	}
 	return 0;
 }
