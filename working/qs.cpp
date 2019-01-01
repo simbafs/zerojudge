2c3fa2f95d74e870754c2434262cdd95c5a9f30f
simbafs <simba.fs@gmail.com>
2019-01-01 09:28:57 +0800

    change something

--- working/qs.cpp
+++ working/qs.cpp
@@ -2,32 +2,32 @@
 
 using namespace std;
 
-int array[100] = {};
+int num[100] = {};
 int n = 10;
 
 int qs(int,int);
 int ex(int*,int*);
 
 int main(){
-	for(int i=0; i<n; i++) cin >> array[i];
+	for(int i=0; i<n; i++) cin >> num[i];
 	
 	cout << "         ";
 	for(int i=0; i<n; i++) cout << i << ' ';
 	cout << endl;
 	cout << "         ";
-	for(int i=0; i<n; i++) cout <<  array[i] << ' ';
+	for(int i=0; i<n; i++) cout <<  num[i] << ' ';
 	cout << endl;
 	
 	qs(0,n-1);
-	for(int i=0; i<n; i++) cout <<  array[i] << ' ';
+	for(int i=0; i<n; i++) cout <<  num[i] << ' ';
 	cout << endl;
 	return 0;
 }
 
 int qs(int head, int tail){
 	if(head < tail && tail-head <= 2){
-		if(array[tail] > array[head]){
-			ex(array+head,array+tail);
+		if(num[tail] > num[head]){
+			ex(num+head,num+tail);
 		}
 		return 0;
 	}
@@ -35,16 +35,23 @@ int qs(int head, int tail){
 		return 0;
 	}
 	int flag = (head+tail)/2;
-	int* indexh = array+head;
-	int* indext = array+tail;
-	while(*indexh < array[flag]) indexh++;
-	while(*indext > array[flag]) indext--;
-	ex(indexh,indext);
+	int* indexh = num+head;
+	int* indext = num+tail;
+	while(indexh <= indext){
+		while(*indexh < num[flag]) indexh++;
+		while(*indext > num[flag]) indext--;
+		ex(indexh,indext);
+		
+		cout << indexh-num << ' ' << indext-num << ' ' << flag << " <> ";
+		for(int i=0;i<n;i++) cout << num[i] << ' ';
+		cout << endl;
+	}
 
-	cout << head << ' ' << tail << ' ' << flag << " >> ";
-	for(int i=0;i<n;i++) cout << array[i] << ' ';
-	cout << endl;
 	
+	cout << head << ' ' << tail << ' ' << flag << " >> ";
+	for(int i=0;i<n;i++) cout << num[i] << ' ';
+	cout << endl;
+
 	qs(head,flag-1);
 	qs(flag+1,tail);
 	return 0;
