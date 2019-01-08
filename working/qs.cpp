31c4adb00a0bdc2090f0ecddfcc9c4fbe8e3c6f4
simbafs <simba.fs@gmail.com>
2019-01-08 12:25:49 +0800

    change something

--- working/qs.cpp
+++ working/qs.cpp
@@ -5,58 +5,25 @@ using namespace std;
 int num[100] = {};
 int n = 10;
 
-int qs(int,int);
+int qs(int,int,int*);
 int ex(int*,int*);
 
 int main(){
-	for(int i=0; i<n; i++) cin >> num[i];
-	
-	cout << "         ";
-	for(int i=0; i<n; i++) cout << i << ' ';
-	cout << endl;
-	cout << "         ";
-	for(int i=0; i<n; i++) cout <<  num[i] << ' ';
-	cout << endl;
-	
-	qs(0,n-1);
-	for(int i=0; i<n; i++) cout <<  num[i] << ' ';
-	cout << endl;
 	return 0;
 }
 
-int qs(int head, int tail){
-	if(head < tail && tail-head <= 2){
-		if(num[tail] > num[head]){
-			ex(num+head,num+tail);
+
+int qs(int head,int tail,int ar[]){
+	if(head >= tail) return 0;
+	if(head-tail <= 2){
+		if(ar[head] >= ar[tail]){
+			ex(ar+head,ar+tail);
 		}
 		return 0;
 	}
-	if(head >= tail){
-		return 0;
-	}
-	int flag = num[(head+tail)/2];
-	int* indexh = num+head;
-	int* indext = num+tail;
-	while(indexh <= indext){
-		while(*indexh <= flag) indexh ++;
-		while(*indext > flag) indext --;
-		ex(indexh,indext);
-		indexh ++;
-		indext --;
-
-		cout << indexh-num << ' ' << indext-num << ' ' << flag << " <> ";
-		for(int i=0;i<n;i++) cout << num[i] << ' ';
-		cout << endl;
-	}
-
+	int flag = at[(head+tail)/2];
 	
-	cout << head << ' ' << tail << ' ' << (head+tail)/2 << " >> ";
-	for(int i=0;i<n;i++) cout << num[i] << ' ';
-	cout << endl;
-	cout << "indexh-num= " << indexh-num << endl
-			 << "indext-num= " << indext-num << endl;
-	qs(head,indexh-num-1);
-	qs(indexh-num,tail);
+
 	return 0;
 }
 
