7684747a5c2c85c6fdd7be1ed4cb992bf3e77482
simba <simba.fs@gmail.com>
2019-03-30 11:37:27 +0800

    working on a121

--- working/a121.cpp
+++ working/a121.cpp
@@ -6,27 +6,29 @@ int main(){
 	int a = 0;
 	int b = 0;
 	int re = 0;
-	cin >> a >> b;
-	if(a == 1) a++;
-	if(b == 1) b++;
-	for(int i = a; i <= b; i++){
-		cout << i << endl;
-		if(i == 2 || i == 3){
-			re ++;
-			cout << "\t\t\t" << i << endl;
-			continue;
-		}
-		for(int j = 2; j*j <= i; j++){
-			cout << "\t" << j << endl;;
-			if(i%j == 0){
-				cout << i << "%" << j << " == 0" << endl; 
-				break;
-			}else if(j*j <= i && (j+1)*(j+1) >= i && i%j != 0){
+	while(cin >> a >> b){
+		re = 0;
+		if(a == 1) a++;
+		if(b == 1) b++;
+		for(int i = a; i <= b; i++){
+//			cout << i << endl;
+			if(i == 2 || i == 3){
 				re ++;
-				cout << "\t\t\t" << i << endl;
+//				cout << "\t\t\t" << i << endl;
+				continue;
+			}
+			for(int j = 2; j*j <= i; j++){
+//				cout << "\t" << j << endl;
+				if(i%j == 0){
+//				cout << "\t" << i << "%" << j << " == 0" << endl; 
+					break;
+				}else if(j*j <= i && (j+1)*(j+1) > i && i%j != 0){
+					re ++;
+//					cout << "\t\t\t" << i << endl;
+				}
 			}
 		}
+		cout << re << endl;
 	}
-	cout << re << endl;
 	return 0;
 }
