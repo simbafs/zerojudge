4ee34f445ac3f7db88e6a86d2ee79ce4e58f6bdd
simba <simba.fs@gmail.com>
2020-03-07 17:32:07 +0800

    a454

--- working/a454.cpp
+++ working/a454.cpp
@@ -10,23 +10,52 @@ struct Node{
 	int n;
 };
 
-/*
-int count(vector <Node> pro, int start){
-	int sum = 0;
+int count(vector <Node> pro, int start, int sum){
+	printf("line 14: start: %d, sum: %d\n", start, sum);
+	auto now = pro[start];
+	sum += now.day;
+	printf("line 18: start: %d, sum: %d\n", start, sum);
+	printf("======\n");
+	if(now.n == 0){
+		printf("line 19: sum: %d\n", sum);
+		return sum;
+	}
+	else if(now.n == 1) return count(pro, now.next[0], sum);
+	else{
+		printf("line 25: sum: %d\n", sum);
+		int b = 0; // biggest
+		for(int i = 0; i < now.n; i++){
+			int t = count(pro, now.next[i], sum);
+			printf("line 29: sum: %d, next: %d\n", sum, now.next[i]);
+			if(t > b) b = t;
+		}
+		printf("line 32: sum: %d, b: %d\n", sum, b);
+//		sum += b;
+		return b;
+	}
+	return sum;
 
+
+	/* 
 	int size = pro.size();
 	for(int i = start; i < size; i++){
-		Sum += pro[i].day;
+		sum += pro[i].day;
 		if(pro[i].n == 0){
 			return sum;
 		}else if(pro[i].n > 1){
-			
+			int biggest = 0;
+			int size = pro[i].size();
+			for(int j = 0; j < size; j++){
+				printf("> i: %d, next: %d\n", i, pro[i][j]);
+				int a = count(pro, j);
+				if(a > biggest) biggest = a;
+			}
+			sum += biggest;
+			return sum;
 		}
 	}
-
-	return sum;
+	*/
 }
-*/
 
 int main(){
 	int _n;
@@ -39,23 +68,17 @@ int main(){
 		for(int i = 0; i < n; i++){
 			Node a;
 			cin >> a.day >> a.n;
-			a.id = i+1;
+			a.id = i;
 			for(int j = 0; j < a.n; j++){
 				int tmp;
 				cin >> tmp;
-				a.next.push_back(tmp);
+				a.next.push_back(tmp - 1);
 			}
 			pro.push_back(a);
 		}
 		
-		vector <Node> wolking;
-		wolking.push_back(pro[0]);
-		while(1){
-				
-		}
-
-
-		printf("%d\n", sum);
+		printf("ok\n");
+		printf("%d\n", count(pro, 0, 0));
 
 		// print
 		/*
